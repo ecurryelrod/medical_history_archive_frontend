9f67eb0fe347403831ea2d8722c68be0de270cdd
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-02-25 17:04:02 -0700

    merged code from displayRecord into displayRecords

--- javascripts/models/medRecord.js
+++ javascripts/models/medRecord.js
@@ -16,26 +16,24 @@ class medRecord {
     }
 
     static displayRecords = (records) => {
-        records.forEach(record => this.displayRecord(record))
-    }
-
-    static displayRecord = (record) => {
-        const div = document.createElement('div')
-        div.className = 'recordBox'
-        div.innerHTML = `
-            <h2 class="category">${record.category.name}</h2>
-            <h4 class="docName">${record.doc_name}</h4>
-            <h4 class="practiceName">${record.practice_name}</h4>
-            <a href="${record.url}" target="_blank">${record.url}</a>
-            <p class="phone">${phoneFormat(record.phone)}</p>
-            <h4>Medications:</h4>
-            <p class="meds">${record.medications}</p>
-            <h4>Medication Notes:</h4>
-            <p class="medNotes">${record.med_notes}</p>
-            <h4>General Comments:</h4>
-            <p class="comments">${record.comments}</p>
-        `
-        recordContainer.append(div)
+        records.forEach(record => {
+            const div = document.createElement('div')
+            div.className = 'recordBox'
+            div.innerHTML = `
+                <h2 class="category">${record.category.name}</h2>
+                <h4 class="docName">${record.doc_name}</h4>
+                <h4 class="practiceName">${record.practice_name}</h4>
+                <a href="${record.url}" target="_blank">${record.url}</a>
+                <p class="phone">${phoneFormat(record.phone)}</p>
+                <h4>Medications:</h4>
+                <p class="meds">${record.medications}</p>
+                <h4>Medication Notes:</h4>
+                <p class="medNotes">${record.med_notes}</p>
+                <h4>General Comments:</h4>
+                <p class="comments">${record.comments}</p>
+            `
+            recordContainer.append(div)
+        })
     }
 }
 
